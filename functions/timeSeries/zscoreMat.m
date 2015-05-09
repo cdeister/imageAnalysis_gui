ed1f5442407ab9c15167a0fb1b1e428a89b07551
Chris Deister <cdeister@brown.edu>
2015-05-09 15:52:38 -0400

    minor change to z-score function

--- functions/timeSeries/zscoreMat.m
+++ functions/timeSeries/zscoreMat.m
@@ -3,14 +3,14 @@ function out=zscoreMat(mat,window)
 % window is a 2 element vector
 
 blv=mean(mat(window(1):window(2),:,:),1);
-blm=repmat(blv,size(mat,1),1,1);
+blm=repmat(blv,[size(mat,1) 1 1]);
 
 
 bld=mat-blm;
 stdD=std(bld(window(1):window(2),:,:),1,1);
 meanD=mean(bld(window(1):window(2),:,:),1);
-blStd=repmat(stdD,size(mat,1),1,1);
-blMean=repmat(meanD,size(mat,1),1);
+blStd=repmat(stdD,[size(mat,1) 1 1]);
+blMean=repmat(meanD,[size(mat,1) 1]);
 
 out=(bld-blMean)./blStd;
 
