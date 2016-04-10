a61dca433e079011310cfac027702aa64b3425b3
Chris Deister <cdeister@brown.edu>
2016-04-10 18:31:48 -0400

    2.9 -- Event Inspector Improvements
    1) eventInspector will now support stimulus triggering and data
    resampling.
    
    ToDo:
    1) Will support application of triggering to a cell, stack, and the
    ability to write out.
    2) More convenient handling of timing variables.

--- functions/timeSeries/stimTrigger.m
+++ functions/timeSeries/stimTrigger.m
@@ -1,4 +1,4 @@
-function out=stimTrigger(dataMatrix,stimTimes,preWindow,postWindow,sampleInterval)
+function [out timeV]=stimTrigger(dataMatrix,stimTimes,preWindow,postWindow,sampleInterval)
 
 preSample=ceil(preWindow/sampleInterval);
 postSample=ceil(postWindow/sampleInterval);
@@ -7,5 +7,8 @@ for i=1:size(dataMatrix,2)
     stimTimes=(stimTimes*(1/sampleInterval))/(1/sampleInterval); % Round the stim such that it is in line with the sample interval.
     stimTimeInSamples=fix(stimTimes(i)/sampleInterval);
     out(:,i)=dataMatrix(stimTimeInSamples-preSample:stimTimeInSamples+postSample,i);
+
     clear('stimTimeInSamples')
 end
+
+timeV=horzcat(-1*fliplr(0:sampleInterval:(preSample*sampleInterval)),sampleInterval:sampleInterval:(postSample*sampleInterval));
